f8899215b086b6632e5819ed1b02802a0b620578
Samuele <samueletasciotti@icloud.com>
2024-07-06 18:27:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<!-- Componenti di pagina -->
+<template>
+  
+  <!-- Dividere in due sezioni la pagina -->
+  <div id="container">
+
+    <!-- LEFT -->
+    <div class="debug"></div>
+
+    <div class="debug"></div>
+  </div>
+</template>
+
+<style scoped>
+
+  /* DEBUG */
+  .debug{
+    width: 400px;
+    height: 400px;
+    
+  }
+</style>
